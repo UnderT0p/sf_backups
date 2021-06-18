d4c36c3cf560a232b3a92d324b7a13b4d4c3743e
UnderT0p <70749855+UnderT0p@users.noreply.github.com>
2021-06-18 18:20:35 +0300

    Update backup_yandex.sh

--- backup_yandex.sh
+++ backup_yandex.sh
@@ -2,6 +2,6 @@
 mkdir /mnt/yandexdisk
 mount -t davfs https://webdav.yandex.ru /mnt/yandexdisk
 cd $1
-tar -cvzf /mnt/yadsk/backup_$(date +%Y-%m-%d-%H-%M).tgz ./
+tar -cvzf /mnt/yandexdisk/backup_$(date +%Y-%m-%d-%H-%M).tgz ./
 umount -f /mnt/yandexdisk
 rm -rf /mnt/yandexdisk
