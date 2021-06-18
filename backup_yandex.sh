82aff8e324c2508254a6e686cd40d4d8f92561af
UnderT0p <70749855+UnderT0p@users.noreply.github.com>
2021-06-18 17:13:13 +0300

    Create backup_yandex.sh

---
+++ backup_yandex.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir /mnt/yandexdisk
+mount -t davfs https://webdav.yandex.ru /mnt/yandexdisk
+cd $1
+tar -cvzf /mnt/yadsk/backup_$(date +%Y-%m-%d-%H-%M).tgz ./
+umount -f /mnt/yandexdisk
+rm -rf /mnt/yandexdisk
